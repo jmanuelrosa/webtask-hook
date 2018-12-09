5696f29cce31185bad621e76130525c909ca98bf
José Manuel Rosa <josemanuel.rosamoncayo+github@gmail.com>
2018-12-09 18:57:18 +0100

    feat: update wip hook with basi middleware

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,16 @@
-module.exports = () => (req, res, next) => {
-  console.log('arguments', arguments)
-  console.log('req', req)
+const error = next => {
+  const error = new Error('Unauthorized App')
+  error.statusCode = 401
+
+  return next(error)
+}
+
+module.exports = () => ({ webtaskContext }, res, next) => {
+  const { secrets } = webtaskContext
+
+  if (!secrets.HOOK_SECRET) {
+    return error(next)
+  }
 
   return next()
-};
+}
--- wip.js
+++ wip.js
@@ -1,5 +1,42 @@
 const octokit = require('@octokit/rest')()
 
+const WIP_CHECK = ['labeled', 'unlabeled', 'edited']
+const WIP = ['\\(wip\\)', '\\[wip\\]', 'wip', 'work\\s*in\\s*progress']
+
+const GIT_CONFIG = {
+  STATUS_MESSAGE: 'This branch is in a work in progress, then you can not merge it',
+  STATUS_CONTEXT: 'Work in progress branch'
+}
+
+const auth = TOKEN => {
+  try {
+    return octokit.authenticate({
+      type: 'oauth',
+      token: TOKEN
+    })
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
+const createStatus = ({ owner, repo }, commitSha, isInWorkInProgress) => {
+  try {
+    return octokit.repos.createStatus({
+      owner,
+      repo,
+      // eslint-disable-next-line camelcase
+      sha: commitSha,
+      state: isInWorkInProgress ? 'error' : 'success',
+      description: GIT_CONFIG.STATUS_MESSAGE,
+      context: GIT_CONFIG.STATUS_CONTEXT
+    })
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
 module.exports = async function ({ data, body }, done) {
   // eslint-disable-next-line camelcase
   const { repository, pull_request } = body
@@ -7,7 +44,21 @@ module.exports = async function ({ data, body }, done) {
 
   try {
     if (WIP_CHECK.includes(body.action)) {
-      console.log('OK')
+      const exps = new RegExp(WIP.join('|'), 'gi')
+
+      const pr = pull_request.labels
+        .map(item => item.name)
+        .concat(pull_request.title)
+
+      const isInWorkInProgress = pr.some(text => exps.test(text))
+
+      await auth(data.GITHUB_TOKEN)
+
+      await createStatus(
+        { owner: login, repo: name },
+        pull_request.head.sha,
+        isInWorkInProgress
+      )
     }
 
     done(null, { result: 'WIP hook Success' })
